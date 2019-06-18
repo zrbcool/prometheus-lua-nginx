69c0d91b5b65d8bac5b6e67c61b7c70dead66414
zhangrongbin <zhangrongbincool@163.com>
2019-06-18 23:09:01 +0800

    we will rock you!!!

---
+++ docker/README.md
@@ -0,0 +1,1 @@
+docker build -t prometheus-lua-nginx:v0.2 -f docker/Dockerfile .
--- workdir/lua/counter.lua
+++ workdir/lua/counter.lua
@@ -1,23 +1,52 @@
-local cjson = require('cjson')
 local pcall = pcall
-local json_decode = cjson.decode
 local ngx = ngx
 local ngx_log = ngx.log
 local ngx_err = ngx.ERR
-local timer_at = ngx.timer.at
-local ngx_sleep = ngx.sleep
-local delay = 300  -- 轮询consul时间间隔，10s
 local _M = {}
-
--- 初始化Prometheus指标，全局字典对象，initted 已经被初始化标记，looped 已经开始循环标记
 function _M.init()
     uris = ngx.shared.uri_by_host
     global_set = ngx.shared.global_set
     global_set:set("initted", false)
     global_set:set("looped", false)
     prometheus = require("prometheus").init("prometheus_metrics") 
-    metric_get_consul = prometheus:counter("nginx_consul_get_total", "Number of query uri from consul", {"status"})
-    metric_latency = prometheus:histogram("nginx_http_request_duration_seconds", "HTTP request latency status", {"host", "status", "scheme", "method", "endpoint"})
+    metric_latency = prometheus:histogram("nginx_http_request_duration_seconds", "HTTP request latency status", {"host", "status", "scheme", "method", "endpoint", "fullurl"})
+end
+local function split(inputstr, sep)
+    if sep == nil then
+        sep = "%s"
+    end
+    local t={} ; i=1
+    for str in string.gmatch(inputstr, "([^"..sep.."]+)") do
+        t[i] = str
+        i = i + 1
+    end
+    return t
+end
+local function parse_fullurl(request_uri)
+    result_table = {}
+    if string.find(request_uri, "%.") ~= nil then
+       return nil
+    end
+    parts = split(request_uri, "/")
+    if table.getn(parts) == 1 then
+       return nil
+    end
+    for j=1, #parts do
+       if(j == 1) then
+           endpoint = "/"..parts[j]
+           fullurl = "/"..parts[j]
+       elseif(j <= 5) then
+           if tonumber(parts[j]) ~= nil then
+               break
+           end
+           fullurl = fullurl.."/"..parts[j]
+       else
+           break
+       end
+    end
+    result_table["endpoint"] = endpoint
+    result_table["fullurl"] = fullurl
+    return result_table
 end
 function _M.log()
     local request_host = ngx.var.host
@@ -25,25 +54,16 @@ function _M.log()
     local request_status = ngx.var.status
     local request_scheme = ngx.var.scheme
     local request_method = ngx.var.request_method
-    local get_all_hosts = uris:get_keys()
-    if get_all_hosts == nil then
-        ngx_log(ngx_err, "Dict is empty！")
+    local remote_ip = ngx.var.remote_addr
+    local ngx_sent = ngx.var.body_bytes_sent
+    local latency = ngx.var.upstream_response_time or 0
+
+
+    result_table = parse_fullurl(request_uri)
+    if result_table == nil then
         return
     end
-    for j=1, #get_all_hosts do
-        if get_all_hosts[j] == request_host then
-            local def_uri = json_decode(uris:get(get_all_hosts[j]))
-            if def_uri == nil then
-                ngx_log(ngx_err, "Decode uris err!")
-                return
-            end
-            for k=1, #def_uri do
-                local s = "^"..def_uri[k].."$"
-                if ngx.re.find(request_uri, s, "isjo" ) ~= nil then
-                    metric_latency:observe(ngx.now() - ngx.req.start_time(), {request_host, request_status, request_scheme, request_method, def_uri[k]})
-                end
-            end
-        end
-    end
+    ngx_log(ngx_err,"latency=", tonumber(latency), ",status=", request_status, ",endpoint=", result_table["endpoint"], ",fullurl=", result_table["fullurl"])
+    metric_latency:observe(tonumber(latency), {request_host, request_status, request_scheme, request_method, result_table["endpoint"], result_table["fullurl"]})
 end
 return _M
